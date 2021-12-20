c1462e30701a9410475a0e163fce7dca7eda9d93
noruwa03 <noruwaobaseki@gmail.com>
2021-12-20 01:00:47 +0100

    saas-hospital-dashboard

---
+++ javascript/app.js
@@ -0,0 +1,18 @@
+const back_btn = document.querySelector(".back_btn");
+const nav = document.querySelector("nav");
+const menu = document.querySelector(".menu");
+
+if (back_btn) {
+  back_btn.onclick = () => {
+    back_btn.classList.toggle("active");
+      nav.classList.toggle("active");
+
+  };
+}
+
+if (menu) {
+  menu.onclick = () => {
+    back_btn.classList.toggle("active");
+      nav.classList.toggle("active");
+  };
+}
